febf83aeddf33587e18823f3211e73d6237bb0f6
ihsankurt17 <102886451+ihsankurt17@users.noreply.github.com>
2022-04-13 00:02:35 +0200

    mrb3

--- src/test/java/onikinisan/OnIkiNisan.java
+++ src/test/java/onikinisan/OnIkiNisan.java
@@ -5,6 +5,7 @@ public class OnIkiNisan {
     public static void main(String[] args) {
 
         System.out.println("Bugünün denemesi - 1");
+        System.out.println("Bugünün denemesi - 2 ve ihsan");
 
     }
 
